539dff8f6eceea9fd2f2640a2e9adf1bc3b4f917
Mesophil <170469614+Quatryl23@users.noreply.github.com>
2026-07-17 11:08:49 +0200

    Fixed Loot Table Wand ignoring Operator Tab being disabled

--- src/main/java/net/mesomods/lootwand/ModItems.java
+++ src/main/java/net/mesomods/lootwand/ModItems.java
@@ -2,6 +2,7 @@ package net.mesomods.lootwand;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.mesomods.lootwand.item.LootTableWandItem;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
@@ -12,6 +13,8 @@ public class ModItems {
     public static final Item LOOT_TABLE_WAND = Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(LootWandMod.MODID, "loot_table_wand"), new LootTableWandItem());
 
     public static void initialize() {
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.OP_BLOCKS).register((group) -> group.accept(LOOT_TABLE_WAND));
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.OP_BLOCKS).register((group) -> {
+            if (Minecraft.getInstance().options.operatorItemsTab().get()) group.accept(LOOT_TABLE_WAND);
+        });
     }
 }
